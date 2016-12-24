1bb86c50a4a9d475ff62062e9b779a72913d0156
zakariarhb <zakariarhb@gmail.com>
2016-12-24 22:42:24 +0100

    un peu de css

--- js/main.js
+++ js/main.js
@@ -5,13 +5,10 @@ window.onload = function(){//empêche l'execution du code avant le chargement de 
 			alert("Impossible de récupérer le canvas");
 			return;
 		}
-		canvas.width = 600;
-		canvas.height = 600;
 	var context = canvas.getContext('2d');
 		if(!context)
 		{
 			alert("Impossible de récupérer le context du canvas");
 			return;
 		}
-	
 }
