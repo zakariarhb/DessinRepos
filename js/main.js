0524dc45790eec1ebaa26db6d50ba56121e8ea64
zakariarhb <zakariarhb@gmail.com>
2016-12-28 18:21:59 +0100

    test canvas element and js Events it important to specifie canvas.width and . in js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,58 @@
 window.onload = function(){//empêche l'execution du code avant le chargement de tous les élts de la page
-	var canvas = document.getElementById('canvas');
+	var canvas = document.querySelector('#canvas');
 		if(!canvas)
 		{
 			alert("Impossible de récupérer le canvas");
 			return;
 		}
+		canvas.width = 700;
+		canvas.height = 600;
 	var context = canvas.getContext('2d');
 		if(!context)
 		{
 			alert("Impossible de récupérer le context du canvas");
 			return;
 		}
+	//button vider
+	var buttonVider = document.getElementById("vider");
+	
+	buttonVider.addEventListener('click',function(){
+		context.clearRect(0,0, canvas.width,canvas.height);//vider à chaque fois notre canvas
+		console.log("vider le canvas");
+	},false);
+	
+	//bouton Dessiner
+	var buttonDessiner = document.getElementById("dessiner");
+	
+	buttonDessiner.addEventListener('click',function(){
+		
+		context.beginPath();
+		context.moveTo(0, 300);
+		context.lineTo(150,0);
+		context.lineTo(300,300);
+		context.lineTo(0,300);
+		context.stroke();
+		console.log("button dessiner");
+	},false);
+	
+	//button enregister
+	var buttonEnregistrer = document.getElementById	("enregistrer");
+	buttonEnregistrer.addEventListener('click',function(){
+		
+	},false);
+	
+	//button Annuler
+	var buttonAnnuler = document.getElementById	("annuler");
+	buttonAnnuler.addEventListener('click',function(){
+		
+	},false);
+	
+	//button trait
+	var buttonTrait = document.getElementById	("trait");
+	buttonTrait.addEventListener('click',function(){
+		context.beginPath();
+		context.moveTo(200, 200);
+		context.lineTo(400,400);
+		context.stroke();
+	},false);
 }
