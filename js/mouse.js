29b3380bcd38c78b1b7481a59cd1bf6c2381374c
zakariarhb <zakariarhb@gmail.com>
2016-12-31 00:47:23 +0100

    fonction de position de la souris et une fct pour la pos. de l elt

---
+++ js/mouse.js
@@ -0,0 +1,28 @@
+﻿function getMousePosition (event){
+	if (event.pageX)//return la position x y sur les deux axe; supporté par tous les nav. sauf IE
+	{
+		return {
+			x: event.pageX ,
+			y: event.pageY
+		};
+	}
+	else{
+		return {
+			x: event.clientX + document.body.scrollLeft + document.documentElement.screenLeft ,
+			y: event.clientY + document.body.scrollTop + document.documentElement.screenTop			
+		};/*l'exception d'Internet Explorer qui demande d'utiliser clientX et
+			clientY. Mais en plus de cela, il faut additionner les valeurs du défilement horizontal et vertical.
+			En effet, sans cela, Internet Explorer ne tient pas compte du défilement de la page*/
+	}
+} 
+//Recuperer les coordonnes de l'elt.
+function getPosition(element){
+	var top =0, left = 0;
+	
+	while (element){
+		left +=element.offsetLeft;
+		top  +=element.offsetTop;
+		element = element.offsetParent;
+	}
+	return {x : left , y: top};
+}
